c587324d0928e01f72403a2e45e7c7cda38d8dde
MerLant <m3rlant@gmail.com>
2025-09-21 13:17:47 +0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,17 @@
 import js from "@eslint/js";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 import solid from "eslint-plugin-solid/configs/recommended";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{ts,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: { globals: globals.browser },
-  },
-  tseslint.configs.recommended,
-  solid,
+	{
+		files: ["**/*.{ts,tsx}"],
+		plugins: { js },
+		extends: ["js/recommended"],
+	},
+	{
+		files: ["**/*.{ts,tsx}"],
+	},
+	tseslint.configs.recommended,
+	solid,
 ]);
